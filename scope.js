519bccfa1c736f76eb59b06a9906302100441f32
hamzamain <mohammadmainuddin211@gmail.com>
2022-09-11 01:14:27 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  const total = a + b;
+  console.log(total);
+  if (a > b) {
+    const sum = a + b;
+  }
+  console.log(sum);
+  return total;
+}
+
+const result = add(15, 10);
+console.log(result);
