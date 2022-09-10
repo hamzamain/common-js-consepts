757aed15f1e9aa7e8412747bb1d7e565202db9f1
hamzamain <mohammadmainuddin211@gmail.com>
2022-09-11 00:47:16 +0600

    double and triple equal

---
+++ triple.js
@@ -0,0 +1,18 @@
+//always use double equal ==
+//equal comparison dosent work for non-primitive
+
+const first = 12 / 2;
+const second = 6;
+if (first === second) {
+  console.log("values are equal");
+} else {
+  console.log("values are not equal");
+}
+
+/* 
+behind the seen of double equal
+
+//type coercion
+//type convertion
+
+*/
